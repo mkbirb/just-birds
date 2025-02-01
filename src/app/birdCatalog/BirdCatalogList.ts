cecf02bcf44643300070207f06e39f09c154e718
mk6520 <s4008599@student.rmit.edu.au>
2025-02-01 20:27:43 +1100

    Did Why Section of Home and Homes Bird Catalog

---
+++ src/app/birdCatalog/BirdCatalogList.ts
@@ -0,0 +1,17 @@
+
+export interface bird {
+  name: string;
+  image: string;
+  }
+
+
+export const BIRDCATALOGLIST: bird[] = [
+
+    { name: 'Budgie', image: 'assets/images/budgie.jpg'},
+    { name: 'Cockatiel', image: 'assets/images/cockatiel.jpg'},
+    { name: 'Lovebird', image: 'assets/images/lovebird.jpg' },
+    { name: 'Canary', image: 'assets/images/canary.jpg'},
+    { name: 'Finch', image: 'assets/images/orange-black-finch.jpg'},
+    { name: 'Meyers Parrot', image: 'assets/images/meyers-parrot.jpg'},
+    { name: 'Pionus Parrot', image:'assets/images/pionus-parrot.jpg'}
+]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { BIRDCATALOGLIST } from '../birdCatalog/BirdCatalogList';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'] 
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  loveBirds = 'assets/images/love-birds.png';
+  cockatoo = 'assets/images/cockatoo.png';
+  petCertificate = 'assets/images/pet-certificate.png';
+
+  birdList = BIRDCATALOGLIST;
+
+  // Displays the reasons why Just Birds should be chosen by the User
+  listOfWhys = [
+    {description: "Cheap birds looking for loving owners!", image: this.loveBirds},
+    {description: "We have a large catalog of many different species of Birds!", image: this.cockatoo},
+    {description: "Gain your Pet Bird Certificate with ease!", image: this.petCertificate}
+  ];
+}
