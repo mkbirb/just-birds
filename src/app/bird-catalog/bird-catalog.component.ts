7d244a9179dc3b8fcde1eae1fd58a5695f3d197b
mk6520 <s4008599@student.rmit.edu.au>
2025-02-07 17:37:15 +1100

    Added Search Functionality to Bird Catalog

--- src/app/bird-catalog/bird-catalog.component.ts
+++ src/app/bird-catalog/bird-catalog.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import { BIRDCATALOGLIST } from '../birdCatalog/BirdCatalogList';
 
 @Component({
   selector: 'app-bird-catalog',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './bird-catalog.component.html',
   styleUrl: './bird-catalog.component.css'
 })
 export class BirdCatalogComponent {
-  productList = BIRDCATALOGLIST
+  noProducts = 'assets/images/no-products.png'
+
+  productList = BIRDCATALOGLIST;
+
+  // Initialise the Filtered List that would appear based on what the User Searched
+  searchedList = BIRDCATALOGLIST;
+
+  searchInput = '';
+
+  //Searches based on User Input
+  searchProduct(event: any) {
+    const query = event.target.value.toLowerCase();
+
+    this.searchedList = this.productList.filter(bird => bird.name.toLowerCase().includes(query));
+  }
+
+  searchReset() {
+    this.searchInput = "";
+
+    // Reset the Searched List
+    this.searchedList = this.productList;
+  }
 }
