d20a7eb3b69a8c47ad2883c33fec41b80c6a775c
mk6520 <s4008599@student.rmit.edu.au>
2025-02-07 22:20:04 +1100

    Did Sorting Bird Catalog

--- src/app/bird-catalog/bird-catalog.component.ts
+++ src/app/bird-catalog/bird-catalog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { BIRDCATALOGLIST } from '../birdCatalog/BirdCatalogList';
 
@@ -8,21 +8,50 @@ import { BIRDCATALOGLIST } from '../birdCatalog/BirdCatalogList';
   templateUrl: './bird-catalog.component.html',
   styleUrl: './bird-catalog.component.css'
 })
-export class BirdCatalogComponent {
+export class BirdCatalogComponent implements OnInit {
   noProducts = 'assets/images/no-products.png'
 
-  productList = BIRDCATALOGLIST;
+  productList = [...BIRDCATALOGLIST];
 
   // Initialise the Filtered List that would appear based on what the User Searched
-  searchedList = BIRDCATALOGLIST;
+  searchedList = [...BIRDCATALOGLIST];
 
   searchInput = '';
 
-  //Searches based on User Input
-  searchProduct(event: any) {
-    const query = event.target.value.toLowerCase();
+  // Set Default to Alphabetical
+  sortOption = 'alphabetical';
 
-    this.searchedList = this.productList.filter(bird => bird.name.toLowerCase().includes(query));
+  ngOnInit(): void {
+    this.sortProducts();
+  }
+
+  //Searches based on User Input
+  searchProduct() {
+    this.searchedList = this.productList.filter(bird =>
+      bird.name.toLowerCase().includes(this.searchInput.toLowerCase()));
+
+    // Done so Sorting is done after the Searching
+    if (this.searchedList.length > 0) {
+      this.sortProducts();
+    }
+  }
+
+  sortProducts() {
+    // If there's a search input, filter the list
+    if (this.searchInput.trim() === '') {
+      // Reset Searched List if there is nothing being searched
+      this.searchedList = [...this.productList];
+    }
+  
+    if (this.sortOption === 'alphabetical') {
+      this.searchedList =  this.searchedList.sort((a,b) => a.name.localeCompare(b.name));
+    }
+    else if (this.sortOption === 'cheapest'){
+      this.searchedList =  this.searchedList.sort((a,b) => a.price - b.price)
+    }
+    else if (this.sortOption === 'highest') {
+      this.searchedList =  this.searchedList.sort((a,b) => b.price - a.price);
+    }
   }
 
   searchReset() {
