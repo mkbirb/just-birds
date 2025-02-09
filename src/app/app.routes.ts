a77e84b8bd8615ab61576f5d557d52e5ec799373
mk6520 <s4008599@student.rmit.edu.au>
2025-02-10 00:38:57 +1100

    Did Page Not Found Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { BirdCatalogComponent } from './bird-catalog/bird-catalog.component';
 import { BirdDetailsComponent } from './bird-details/bird-details.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'bird-catalog', component: BirdCatalogComponent},
-  { path: 'bird/:id', component: BirdDetailsComponent}
+  { path: 'bird/:id', component: BirdDetailsComponent},
+  { path: '**', component: NotFoundComponent}
 ];
---
+++ src/app/not-found/not-found.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.css'
+})
+export class NotFoundComponent {
+  notFoundPicture = 'assets/images/page-not-found.png';
+  
+  router = inject(Router);
+
+  backToHome() {
+    this.router.navigateByUrl('/home')
+  }
+}
