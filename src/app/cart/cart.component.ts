1a8d1ff72f6ad5ed5542004e68f9351466e75c60
mk6520 <s4008599@student.rmit.edu.au>
2025-02-14 12:29:52 +1100

    Did Shopping Cart Styling

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -9,12 +10,25 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit{
     cart: any[] = [];
-    constructor(private cartService: CartService){};
+    constructor(private cartService: CartService, private router: Router){};
+
+    finalPrice: number = 0;
+
+    noItemsPicture = 'assets/images/empty-cart.png'
 
     ngOnInit(): void {
       this.cartService.cart$.subscribe(items => {
         this.cart = items;
       })
+      this.calculateFinalPrice();
+    }
+
+    toCatalog() {
+      this.router.navigateByUrl('/bird-catalog');
+    }
+
+    calculateFinalPrice() {
+      this.finalPrice = this.cart.reduce((sum, product) => sum + (product.item.price * product.quantity), 0);
     }
 
     removeItem(index: number) {
